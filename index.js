9705ef6b98c2001884e8e515c8bf5f18e3b73e23
Mya <211755169+eri010-tech@users.noreply.github.com>
2026-01-07 17:23:14 -0800

    focus on event listener next

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 //console.log("sweet floating in the ether"); 
 const gridContainer = document.querySelector("#grid-container"); 
 
-/*function makeGrid(){
-  for(let i = 0; i < 16; i++){
-    const row = document.createElement("div");
+//makeGrid function utilizes nested loops to create a dynamic grid. 
+// I couldn't figure this out myself, so the answer comes from stack overflow. 
+
+function makeGrid(size){
+  for(let i = 0; i < size; i++){ //outer loop is purple 
+    let row = document.createElement("div"); 
     row.classList.add("row"); 
-    gridContainer.appendChild(row);
+    gridContainer.appendChild(row); 
     row.textContent = i; 
-
-    for (let j = 1; j < 16; j++){
-      const column = document.createElement("div"); 
-      column.classList.add("column"); 
-      row.appendChild(column); 
-      column.textContent = j; 
+    for(let j = 1; j < size; j++){ // inner loop is blue 
+    let column = document.createElement("div"); 
+    column.classList.add("column"); 
+    row.appendChild(column); 
+    column.textContent = j; 
     }
-  }
+  } 
 }
-//talk about why the loops work?!
-makeGrid(); 
-*/
-
-function makeGrid(){
-  for(let i = 0; i < 4; i++){ //outer loop is purple 
-    for(let j = 0; j < 4; i++){ // inner loop is blue 
-       console.log(i,j);
-    }
-  }
-}
-makeGrid();
+makeGrid(16); 
