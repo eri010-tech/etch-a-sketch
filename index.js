252be3903390173761ac8245b49e1ece52f3fa75
Mya <211755169+eri010-tech@users.noreply.github.com>
2026-01-08 09:59:55 -0800

    need to research grid

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const gridContainer = document.querySelector("#grid-container");
 
 //makeGrid function utilizes nested loops to create a dynamic grid. 
 // I couldn't figure this out myself, so the answer comes from stack overflow. 
-/*
+
 function makeGrid(size){
   for(let i = 0; i < size; i++){ //outer loop is purple 
     let row = document.createElement("div"); 
@@ -18,22 +18,4 @@ function makeGrid(size){
     }
   } 
 }
-makeGrid(16); */
-
-
-
-function makeGrid(size){
-  for(let i = 0; i < size; i++){ //outer loop is purple 
-    let column = document.createElement("div"); 
-    column.classList.add("column");  
-    // talk about having to not make "column.textContent = i;" visible so that grid has even borders.  
-    for(let j = 0; j < size; j++){ // inner loop is blue 
-    let row = document.createElement("div"); 
-    row.classList.add("row"); 
-    column.appendChild(row);
-    gridContainer.appendChild(column); 
-    row.textContent = j; 
-    }
-  } 
-}
-makeGrid(16);
+makeGrid(16); 
