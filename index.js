d51ff884416d8c10388c84ece82aeea5566510f3
Mya <211755169+eri010-tech@users.noreply.github.com>
2026-01-09 15:54:56 -0800

    figured out random rgb boxes

--- index.js
+++ index.js
@@ -12,18 +12,44 @@ const gridSketchpad = document.querySelector(".sketchpad");
 gridSketchpad.style.width = `${gridSide}px`;
 gridSketchpad.style.height = `${gridSide}px`; 
 
+
 function makeGridSquares() {
   for(let i = 0; i < (rows * columns); i++){
-    const gridPiece = document.createElement("div");
+   const gridPiece = document.createElement("div");
 
     gridPiece.style.width = `${(gridSide / columns) - 2}px`;
     gridPiece.style.height = `${(gridSide / rows) - 2}px`;
     gridPiece.classList.add("gridSquare");
-
+    
     gridSketchpad.appendChild(gridPiece); 
   }
 }
-
+ 
 makeGridSquares();
 
-//talk about why this works!!!
+
+//event listener for randomly changing the grid colors 
+
+gridSketchpad.addEventListener('mouseover', changeGridColors); 
+
+
+function changeGridColors(e) {
+  let box = e.target; 
+  let color = randomRGBColor(box); 
+
+  box.style.backgroundColor = color; 
+}
+
+// randomRGBColor function from YouTuber Tech With Tim 
+
+function randomRGBColor(){
+  let red = Math.round(Math.random() * 255); 
+  let green = Math.round(Math.random() * 255); 
+  let blue = Math.round(Math.random() * 255); 
+
+  let randomColor = `rgb(${red}, ${green}, ${blue})`; 
+  return randomColor; 
+}
+
+
+
