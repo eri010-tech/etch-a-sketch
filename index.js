10b0164b9c686f5296cdaff2ea5c82e2b80b7bfc
Mya <211755169+eri010-tech@users.noreply.github.com>
2026-01-12 18:47:56 -0800

    working on requirement logic

--- index.js
+++ index.js
@@ -2,95 +2,6 @@
 
 /* I couldn't figure this out myself, so all the grid code is from 
 a YouTube channel called: Marvin Botchway!!! */
-/* comment starts here 
-const gridSide = 555; 
-let rows = 16;
-let columns = 16; 
-
-const gridSketchpad = document.querySelector(".sketchpad");
-gridSketchpad.style.width = `${gridSide}px`;
-gridSketchpad.style.height = `${gridSide}px`; 
-
-
-function makeGridSquares() {
-  for(let i = 0; i < (rows * columns); i++){
-   const gridPiece = document.createElement("div");
-
-    gridPiece.style.width = `${(gridSide / columns) - 2}px`;
-    gridPiece.style.height = `${(gridSide / rows) - 2}px`;
-    gridPiece.classList.add("gridSquare");
-    
-    gridSketchpad.appendChild(gridPiece); 
-  }
-}
- 
-makeGridSquares();
-
-comment ends here */ 
-//event listener for randomly changing the grid colors 
-/* commenr starts here 
-gridSketchpad.addEventListener('mouseover', changeGridColors); 
-
-
-function changeGridColors(e) {
-  let box = e.target; 
-  let color = randomRGBColor(box); 
-
-  box.style.backgroundColor = color; 
-}
-comment ends here */ 
-// randomRGBColor function from YouTuber Tech With Tim 
-/* comment starts here 
-function randomRGBColor(){
-  let red = Math.round(Math.random() * 255); 
-  let green = Math.round(Math.random() * 255); 
-  let blue = Math.round(Math.random() * 255); 
-
-  let randomColor = `rgb(${red}, ${green}, ${blue})`; 
-  return randomColor; 
-}
-comment ends here  */ 
-
-// "Click Button" event listener will prompt the user to pick a row/column number between
-// 0-100. The grid should then change to the designated grid size.
-/* comment starts here 
-const clickBtn = document.querySelector("#click-btn"); 
-
-clickBtn.addEventListener('click', changeGrid); 
-
-
-function changeGrid(){
-  
-  let command = prompt("please pick a grid number between 1 and 100");
-  let numberPicked = command; 
-  
-  check = checkUserInput(numberPicked); 
-  console.log(check); 
-   
-} 
-
-function checkUserInput(numberPicked){
-  if (numberPicked > 100){
-    alert("maximumm number allowed is 100. please click the button again to type a new number");
-  } else if (numberPicked < 1){
-    alert("minimum number allowed is 1. please click the button again to type a new number");
-  } else {
-    return numberPicked; 
-  }
-}
-/*comment ends here 
-
-
-
-
-
-
-// ###### below is my experiment ######## //
-
-//console.log("sweet floating in the ether"); 
-
-/* I couldn't figure this out myself, so all the grid code is from 
-a YouTube channel called: Marvin Botchway!!! */
 
 let tools = {
 gridSide: 555, 
@@ -98,16 +9,6 @@ rows: 16,
 columns: 16 
 }
 
-console.log(typeof tools);
-console.log(tools.gridSide);
-console.log(tools.rows);
-console.log(tools.columns);
-
-/*
-const gridSide = 555; 
-let rows = 16;
-let columns = 16; 
-*/
 const gridSketchpad = document.querySelector(".sketchpad");
 gridSketchpad.style.width = `${tools.gridSide}px`;
 gridSketchpad.style.height = `${tools.gridSide}px`; 
@@ -129,7 +30,7 @@ makeGridSquares();
 
 
 //event listener for randomly changing the grid colors 
-
+ 
 gridSketchpad.addEventListener('mouseover', changeGridColors); 
 
 
@@ -199,7 +100,14 @@ function checkUserInput(numberPicked){
     return numberPicked; 
   }
 }
-// to do: ### remove duplicate of code #### . make sure there isn't any bugs.
+
+
+
+
+
+
+
+// to do: 
 // check odin website to make sure  you've done everything required for the project. 
 // maybe do extra credit?
 
